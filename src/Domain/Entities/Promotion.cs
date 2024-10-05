6fe279e8c60a4edac58c333543147ff0ae395b34
@giangnntse173469 <giangnntse173469@fpt.edu.vn>
2024-10-05 13:56:14 +0700

    batch, promotion

---
+++ src/Domain/Entities/Promotion.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KFS.src.Domain.Entities
+{
+    public class Promotion
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public string? PromotionCode { get; set; }
+        public int DiscountPercentage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool IsActive { get; set; }
+        public List<Product> Products { get; set; } = new List<Product>();
+        public List<Batch> Batches { get; set; } = new List<Batch>();
+        public List<Category> Categories { get; set; } = new List<Category>();
+    }
+}
