19c7013dafcf1391793835d780278ab0bec0bdd4
unknown <loclhse172459@fpt.edu.vn>
2024-10-18 11:23:55 +0700

    latest

--- api-tests/steps_file.js
+++
@@ -1,8 +0,0 @@
-module.exports = function() {
-  return actor({
-    // Define custom steps here, for example:
-    // login: function(email, password) {
-    //   this.sendPostRequest('/login', {email, password});
-    // }
-  });
-}
