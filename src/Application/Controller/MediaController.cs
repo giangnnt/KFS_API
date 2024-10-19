c5dddbcdef9b01c0c78374f05c30544d24ff7a9c
Calypso <giangnnt260703@gmail.com>
2024-10-19 08:39:04 +0700

    media controller

---
+++ src/Application/Controller/MediaController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KFS.src.Application.Controller
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("api/media")]
+    public class MediaController : ControllerBase
+    {
+        
+    }
+}
