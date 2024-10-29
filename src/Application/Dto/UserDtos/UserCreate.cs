ccd9af9b44aa196fbc9a010456365e87c9861eb1
Calypso <giangnnt260703@gmail.com>
2024-10-29 16:49:16 +0700

    user api update

--- src/Application/Dto/UserDtos/UserCreate.cs
+++ src/Application/Dto/UserDtos/UserCreate.cs
@@ -1,14 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using KFS.src.Application.Constant;
 
 namespace KFS.src.Application.Dto.UserDtos
 {
     public class UserCreate
     {
+
+        [Required]
+        [StringLength(30)]
         public string FullName { get; set; } = null!;
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(RegexConst.EMAIL, ErrorMessage = "Invalid email address")]
         public string Email { get; set; } = null!;
+        [Required]
+        [StringLength(15, ErrorMessage = "Phone number must be 0-15 characters")]
+        [RegularExpression(RegexConst.PHONE_NUMBER, ErrorMessage = "Invalid phone number")]
         public string Phone { get; set; } = null!;
         public int RoleId { get; set; }
     }
